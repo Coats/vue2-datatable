fae2e961001ae6ad9ef1fe6a86cb26288c141359
Ovidiu Enescu <ovidiu.enescu@tremend.com>
2018-11-27 09:30:56 +0200

    implemented column reordering on table header

--- plugins/HeaderSettingsDnD.js
+++ plugins/HeaderSettingsDnD.js
@@ -15,7 +15,7 @@
     ...
   }
   </script>
- 
+
  * `vm.columns` should also meet the requirement that:
  * the same-group columns should be put together
  * e.g.
@@ -39,38 +39,74 @@
  * @param {VueInstance} vm
  */
 export default function dnd(vm) {
-  const $ColGroupContainer = $(vm.$el).find('div.-col-group-container')
-  const DRAGGABLE = 'li[draggable=true]'
-  const DROP_ZONE = 'li.-col-drop-zone'
-  
+  const $TheadContainer = $(vm.$el).find('thead')
+  const $TbodyContainer = $(vm.$el).find('tbody')
+  const DRAGGABLE = 'th[draggable=true]'
+  const DROP_ZONE = 'th.-col-drop-zone'
+
+  /** generates <th> element with class '-col-drop-zone' */
   function dropZoneGen(idx) {
-    return `<li class="-col-drop-zone" target-idx="${idx}"></li>`
+    return `<th class="-col-drop-zone" target-idx="${idx}"></th>`
   }
-  /** generate adjacent drop zones for each column displayed as <li> */
+
+  /** generates extra <td> with class 'temp' */
+  function extraSpaceTdGen(idx) {
+    return `<td class="temp" target-idx="${idx}"></td>`
+  }
+
+  /** sets <th> elements draggable true and maps the attr 'source-idx' according to the column it
+    represents; useful when hiding certain columns without affecting drag reorder */
+  function setDraggableThAndMapIdx() {
+    let {columns} = vm
+    $TheadContainer.find('th').each(function() {
+      let $this = $(this)
+
+      let textValue = $this.text().trim();
+      let index = columns.findIndex(function(el) {
+        return el.title === textValue
+      })
+
+      $this.attr('draggable', true)
+      $this.attr('source-idx', index)
+    })
+  }
+
+  /** generate adjacent drop zones for each column displayed as <th> */
   function generateDropZones() {
-    $ColGroupContainer
+    $TheadContainer
       .find(DROP_ZONE).remove().end() // ensure no drop zone exists
-      .find('li').each(function (idx) {
+      .find('th').each(function () {
         const $this = $(this)
+        let idx = $this.attr('source-idx')
         $this
-          .attr('draggable', 'true')
-          .attr('source-idx', idx)
           .before(
             dropZoneGen(
-              $this.is('li:first-of-type') ? idx - 0.25 : idx
+              $this.is('th:first-of-type') ? idx - 0.25 : idx
             )
           )
 
-        $this.is('li:last-of-type') && $this.after(dropZoneGen(idx + 0.25))
+        $this.is('th:last-of-type') && $this.after(dropZoneGen(idx + 0.25))
+      })
+    /** also generate empty <td> elements on the body for header-column match */
+    $TbodyContainer
+      .find('td').each(function () {
+        const $this = $(this)
+        $this
+          .before(extraSpaceTdGen())
       })
   }
 
+  /** removes generated <th> elements on the header and also body empty <td> */
+  function removeDropZones() {
+    $TheadContainer.find(DROP_ZONE).remove()
+    $TbodyContainer.find('.temp').remove()
+  }
+
   vm.$watch('columns', () => {
-    vm.$nextTick(generateDropZones)
-  }, { immediate: true, deep: true })
+    vm.$nextTick(setDraggableThAndMapIdx)
+  }, {immediate: true, deep: true})
 
-  /*** ↑↑↑ preparatory work --- main logic ↓↓↓ ***/
-
+  /**  ↑↑↑ preparatory work --- main logic ↓↓↓ ***/
   let draggingIdx = null
 
   $.fn.isAllowedToDrop = function () {
@@ -78,13 +114,15 @@ export default function dnd(vm) {
     return ![-0.25, 0, 0.25, 1].includes(targetIdx - draggingIdx) // filter adjacent drop zones
   }
 
-  $ColGroupContainer
+  $TheadContainer
     .on('dragstart', DRAGGABLE, function () {
+      generateDropZones()
       draggingIdx = +$(this).addClass('-dragging').attr('source-idx')
     })
     .on('dragend', DRAGGABLE, function () {
       draggingIdx = null
       $(this).removeClass('-dragging')
+      removeDropZones()
     })
     .on('dragover', DROP_ZONE, function (e) {
       e.preventDefault() // must
@@ -100,8 +138,9 @@ export default function dnd(vm) {
 
       const { columns } = vm
       const targetIdx = +$this.attr('target-idx')
-      columns[draggingIdx].group = columns[Math.round(targetIdx)].group
+
       arrMove(columns, draggingIdx, Math.ceil(targetIdx))
+      removeDropZones()
     })
     .on('dragleave', DROP_ZONE, function () {
       const $this = $(this)
@@ -111,7 +150,10 @@ export default function dnd(vm) {
 
 // similar to https://github.com/sindresorhus/array-move
 function arrMove(arr, from, to) {
-  arr.splice((from < to ? to - 1 : to), 0, arr.splice(from, 1)[0])
+  /*let index = (from < to ? to - 1 : to)
+  let itemToMove = arr.splice(from, 1)[0]
+  arr.splice(index, 0, itemToMove)*/
+    arr.splice((from < to ? to - 1 : to), 0, arr.splice(from, 1)[0])
 }
 
 $('head').append(`<style>
@@ -123,12 +165,31 @@ $('head').append(`<style>
     opacity: 0.3;
   }
   .-col-drop-zone {
-    height: 10px;
+    height: 10px;    
+    background: lightgray;
     transition: height .25s ease;
+  }
+  .-col-drop-zone:hover {
+    background: #337ab7;
+    transition: background .5s;
   }
   .-droppable {
     height: 30px;
     border: 1px dashed #ddd;
     border-radius: 4px;
+    background: #337ab7;
+    -webkit-transition: .4s;
+    -moz-transition: .4s;
+    -ms-transition: .4s;
+    -o-transition: .4s;
+    transition: .4s;
   }
+  th[draggable=true] {
+  cursor: move;
+  z-index: 100;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;  
+  }  
 </style>`)
